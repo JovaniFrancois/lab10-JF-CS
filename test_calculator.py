5cae27a7376692f19ba5c9b24b509f17bfc68607
jovani <jovanifrancois26@gmail.com>
2025-11-14 13:57:08 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/lab10-JF-CS.iml
    #	.idea/misc.xml
    #	calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -21,9 +21,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.mul(-2, 5), -10)
         self.assertEqual(calculator.mul(0, 100), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(calculator.div(10, 2), 5)
+        self.assertAlmostEqual(calculator.div(7, 3), 7 / 3)
+        self.assertEqual(calculator.div(-8, 4), -2)
+    ###########################
 
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
@@ -41,21 +43,22 @@ class TestCalculator(unittest.TestCase):
 
 
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.log(2, 0)
+        with self.assertRaises(ValueError):
+            calculator.log(2, -5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13)
+        self.assertEqual(calculator.hypotenuse(0, 0), 0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self):
+        self.assertEqual(calculator.square_root(9), 3)
+        self.assertEqual(calculator.square_root(0), 0)
+        with self.assertRaises(ValueError):
+            calculator.square_root(-1)
     ##########################
 
 # Do not touch this
