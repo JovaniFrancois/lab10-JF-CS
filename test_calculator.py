15268926c50533217776107beb9a2c22bcaeacae
jovani <jovanifrancois26@gmail.com>
2025-11-14 00:15:47 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/lab10-JF-CS.iml
    #	.idea/misc.xml
    #	calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,59 +1,62 @@
 import unittest
 import calculator
 
-
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(calculator.add(1, 2), 3)
+        self.assertEqual(calculator.add(5, 3), 8)
+        self.assertEqual(calculator.add(1, 9), 10)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(calculator.sub(5, 3), 2)
+        self.assertEqual(calculator.sub(5, 4), 1)
+        self.assertEqual(calculator.sub(10, 6), 4)
+
 
     ######## Partner 1
-    def test_multiply(self):
+     def test_multiply(self):
         self.assertEqual(calculator.mul(3, 4), 12)
         self.assertEqual(calculator.mul(-2, 5), -10)
         self.assertEqual(calculator.mul(0, 100), 0)
 
-    def test_divide(self):
-        self.assertEqual(calculator.div(10, 2), 5)
-        self.assertAlmostEqual(calculator.div(7, 3), 7 / 3)
-        self.assertEqual(calculator.div(-8, 4), -2)
-
-    ##########################
+    # def test_divide(self): # 3 assertions
+    #     fill in code
+    # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
+    def test_divide_by_zero(self): # 1 assertion
+        self.assertRaises(ZeroDivisionError, div, 0, 5)
+
+
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(calculator.log(2, 2), 1)
+        self.assertEqual(calculator.log(100, 10), 2)
+        self.assertAlmostEqual(calculator.log(8, 2), 3)
     #     fill in code
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_log_invalid_base(self): # 1 assertion
+        self.assertRaises(ValueError, calculator.log, 10, 0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     fill in code
-    ##########################
 
     ######## Partner 1
-    def test_log_invalid_argument(self):
-        with self.assertRaises(ValueError):
-            calculator.log(2, 0)
-        with self.assertRaises(ValueError):
-            calculator.log(2, -5)
+    # def test_log_invalid_argument(self): # 1 assertion
+    #     # call log function inside, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #     logarithm(0, 5)
+    #     fill in code
 
-    def test_hypotenuse(self):
-        self.assertEqual(calculator.hypotenuse(3, 4), 5)
-        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13)
-        self.assertEqual(calculator.hypotenuse(0, 0), 0)
+    # def test_hypotenuse(self): # 3 assertions
+    #     fill in code
 
-    def test_sqrt(self):
-        self.assertEqual(calculator.square_root(9), 3)
-        self.assertEqual(calculator.square_root(0), 0)
-        with self.assertRaises(ValueError):
-            calculator.square_root(-1)
+    # def test_sqrt(self): # 3 assertions
+    #     # Test for invalid argument, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #    square_root(NUM)
+    #     # Test basic function
+    #     fill in code
     ##########################
-
 
 # Do not touch this
 if __name__ == "__main__":
